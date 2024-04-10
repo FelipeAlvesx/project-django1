c4e84cf4082a03f8dd0f3dfd23cc12adf65e6d91
FelipeAlvesx <feelipe.devloper@gmail.com>
2024-04-10 14:44:25 -0300

    namespaces

--- recipes/views.py
+++ recipes/views.py
@@ -5,11 +5,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    context = {}
-    return render(request, "recipes/home.html", context=context)
+    context = {"name": "felipe"}
+    return render(request, "recipes/home.html", context=context, status=201)
 
 def sobre(request):
-    return HttpResponse("SOBRE")
+    return render(request, "contato.html", context={})
 
 def contato(request):
     return HttpResponse("CONTATO")
