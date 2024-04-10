d05dc99f81fb8416dc2b13f330ba8369ebffaea5
FelipeAlvesx <feelipe.devloper@gmail.com>
2024-04-10 14:31:47 -0300

    namespace_template_folders

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("HOME")
+    context = {}
+    return render(request, "recipes/home.html", context=context)
 
 def sobre(request):
     return HttpResponse("SOBRE")
